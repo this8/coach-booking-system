c00066397feb458c5ffb81dc268e46e9409008d3
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2022-03-24 02:23:36 +0530

    Connections php code updated

--- connection.php
+++ connection.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$db_name = "coach_booking_system";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $db_name);
 
 // Check connection
 if (!$conn) {
